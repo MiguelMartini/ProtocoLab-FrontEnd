9e00b3eed0382f6853cfd3539133d001a7c3f74f
MiguelMartini <miguelkaulingm21@gmail.com>
2026-06-14 14:15:05 -0300

    Feat: Register Page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterFeature from '../features/Auth/RegisterFeature'
+
+function Register() {
+  return (
+    <div>
+      <RegisterFeature/>
+    </div>
+  )
+}
+
+export default Register
