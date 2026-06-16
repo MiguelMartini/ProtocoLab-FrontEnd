2e6cbdc297eb53e4a2be7735dcd5ab241f39d9a0
MiguelMartini <miguelkaulingm21@gmail.com>
2026-06-15 21:57:10 -0300

    Feat: Adicao de cards estatisticos

--- src/features/Dashboard/DashBoardFeature.jsx
+++ src/features/Dashboard/DashBoardFeature.jsx
@@ -1,12 +1,29 @@
-import TicketFeature from "../Tickets/TicketFeature"
+import TicketFeature from "../Tickets/TicketFeature";
+import DashBoardCard from "./DashBoardCard";
 
 function DashBoardFeature() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <TicketFeature/>
+    <div className="px-20 pt-10 max-w-7xl mx-auto">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold text-slate-900">Dashboard</h1>
+
+        <div className="mt-2">
+          <h2 className="text-2xl text-slate-600">
+            Olá, <span className="font-bold text-[#4F39F6]">User</span>
+          </h2>
+
+          <p className="text-md text-slate-500 mt-1">
+            Acompanhe seus chamados e atividades recentes.
+          </p>
+        </div>
+      </div>
+      <div className="flex gap-5">
+        <DashBoardCard title={"Chamados"} total={13} description={"Total de chamados"}/>
+        <DashBoardCard title={"Abertos"} total={3} description={"Chamados abertos"}/>
+      </div>
+      <TicketFeature />
     </div>
-  )
+  );
 }
 
-export default DashBoardFeature
+export default DashBoardFeature;
