987f84eeeb70294bfd30047af6d2f60dac31a3c5
MiguelMartini <miguelkaulingm21@gmail.com>
2026-06-22 11:39:11 -0300

    Feat: Equipe page

---
+++ src/pages/Team.jsx
@@ -0,0 +1,13 @@
+import TeamFeature from "@/features/Team/TeamFeature"
+import TeamHeader from "@/features/Team/TeamHeader"
+
+function Team() {
+  return (
+    <div>
+        
+        <TeamFeature/>
+    </div>
+  )
+}
+
+export default Team
