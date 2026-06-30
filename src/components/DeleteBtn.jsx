0e555ea4552c5a66a890eb1b82d129997649c673
MiguelMartini <miguelkaulingm21@gmail.com>
2026-06-29 23:12:37 -0300

    Feat: Delete Btn internacionalizado

--- src/components/DeleteBtn.jsx
+++ src/components/DeleteBtn.jsx
@@ -1,14 +1,16 @@
+import { useTranslation } from 'react-i18next';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from './ui/alert-dialog'
 import { Button } from './ui/button'
 import { Trash2 } from 'lucide-react'
 
 function DeleteBtn({ msg, alertMsg,onDelete }) {
+  const { t } = useTranslation();
    return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
         <Button variant="destructive" size='lg' className={"cursor-pointer"}>
           <Trash2 className="w-4 h-4 mr-2" />
-          Deletar
+          {t("deleteBtn.delBtn")}
         </Button>
       </AlertDialogTrigger>
 
@@ -24,13 +26,13 @@ function DeleteBtn({ msg, alertMsg,onDelete }) {
         </AlertDialogHeader>
 
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogCancel>{t("deleteBtn.cancelBtn")}</AlertDialogCancel>
 
           <AlertDialogAction
             onClick={onDelete}
             className="bg-red-600 hover:bg-red-700"
           >
-            Excluir
+            {t("deleteBtn.delBtn")}
           </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
