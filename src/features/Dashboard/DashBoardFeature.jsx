5b0eb830e6a97f639fa95aa82954f2af5ed5f4a8
MiguelMartini <miguelkaulingm21@gmail.com>
2026-06-30 23:22:11 -0300

    Feat: cards dashboard

--- src/features/Dashboard/DashBoardFeature.jsx
+++ src/features/Dashboard/DashBoardFeature.jsx
@@ -4,84 +4,128 @@ import DashBoardCard from "./DashBoardCard";
 import { useTranslation } from "react-i18next";
 import { useAuth } from "@/hooks/useAuth";
 import {
-	ticketStatusData,
-	ticketPriorityData,
-	ticketDepartmentData,
-	ticketMonthlyData,
+  ticketStatusData,
+  ticketPriorityData,
+  ticketDepartmentData,
+  ticketMonthlyData,
 } from "@/mocks/ticketChart";
 
 //Para fazer a navegação do gráfico de barra.
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getTickets } from "@/api/tickets.api";
 
 function DashBoardFeature() {
-	const { t } = useTranslation();
-	const { user } = useAuth();
+  const { t } = useTranslation();
+  const { user } = useAuth();
+  const [ticketCount, setTicketCount] = useState(0);
 
-	const navigate = useNavigate();
+  const [stats, setStats] = useState({
+    total: 0,
+    open: 0,
+    inProgress: 0,
+    closed: 0,
+  });
 
-	const handleSliceClick = (entry) => {
-		console.log(entry);
+  useEffect(() => {
+    async function loadTickets() {
+      try {
+        const tickets = await getTickets();
 
-		// Exemplo de redirect
-		navigate(`/tickets?priority=${entry.priority}`);
-	};
+        setStats({
+          total: tickets.length,
+          open: tickets.filter((ticket) => ticket.status === "OPEN").length,
+          inProgress: tickets.filter(
+            (ticket) => ticket.status === "IN_PROGRESS",
+          ).length,
+          closed: tickets.filter((ticket) => ticket.status === "CLOSED").length,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
-	return (
-		<div className="px-8 pt-10 max-w-7xl mx-auto">
-			<div className="mb-8">
-				<h1 className="text-3xl font-bold text-slate-900">
-					{t("dashboard.title")}
-				</h1>
+    loadTickets();
+  }, []);
+  useEffect(() => {
+    async function loadTickets() {
+      try {
+        const tickets = await getTickets();
 
-				<div className="mt-2">
-					<h2 className="text-2xl text-slate-600">
-						{t("dashboard.greeting")}{" "}
-						<span className="font-bold text-[#4F39F6]">{user?.name}</span>
-					</h2>
+        setTicketCount(tickets.length);
+      } catch (error) {
+        console.error("Erro ao buscar tickets:", error);
+      }
+    }
 
-					<p className="text-md text-slate-500 mt-1">
-						{t("dashboard.subtitle")}
-					</p>
-				</div>
-			</div>
+    loadTickets();
+  }, []);
 
-			<div className="flex gap-5">
-				<DashBoardCard
-					title={t("dashboard.cards.tickets.title")}
-					total={13}
-					description={t("dashboard.cards.tickets.description")}
-				/>
+  const navigate = useNavigate();
 
-				<DashBoardCard
-					title={t("dashboard.cards.open.title")}
-					total={3}
-					description={t("dashboard.cards.open.description")}
-				/>
+  const handleSliceClick = (entry) => {
+    console.log(entry);
 
-				<DashBoardCard
-					title={t("dashboard.cards.progress.title")}
-					total={8}
-					description={t("dashboard.cards.progress.description")}
-				/>
+    // Exemplo de redirect
+    navigate(`/tickets?priority=${entry.priority}`);
+  };
 
-				<DashBoardCard
-					title={t("dashboard.cards.closed.title")}
-					total={19}
-					description={t("dashboard.cards.closed.description")}
-				/>
-			</div>
+  return (
+    <div className="px-8 pt-10 max-w-7xl mx-auto">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold text-slate-900">
+          {t("dashboard.title")}
+        </h1>
 
-			<div className="mt-8 space-y-5">
-				<div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-					<TicketChart title="Chamados por status" data={ticketStatusData} />
-					<TicketChart
-						title="Chamados por prioridade"
-						data={ticketPriorityData}
-						onSliceClick={handleSliceClick}
-					/>
-				</div>
+        <div className="mt-2">
+          <h2 className="text-2xl text-slate-600">
+            {t("dashboard.greeting")}{" "}
+            <span className="font-bold text-[#4F39F6]">{user?.name}</span>
+          </h2>
 
-				{/* <TicketChart
+          <p className="text-md text-slate-500 mt-1">
+            {t("dashboard.subtitle")}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex gap-5">
+        <DashBoardCard
+          title={t("dashboard.cards.tickets.title")}
+          total={stats.total}
+          description={t("dashboard.cards.tickets.description")}
+        />
+
+        <DashBoardCard
+          title={t("dashboard.cards.open.title")}
+          total={stats.open}
+          description={t("dashboard.cards.open.description")}
+        />
+
+        <DashBoardCard
+          title={t("dashboard.cards.progress.title")}
+          total={stats.inProgress}
+          description={t("dashboard.cards.progress.description")}
+        />
+
+        <DashBoardCard
+          title={t("dashboard.cards.closed.title")}
+          total={stats.closed}
+          description={t("dashboard.cards.closed.description")}
+        />
+      </div>
+
+      <div className="mt-8 space-y-5">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          <TicketChart title="Chamados por status" data={ticketStatusData} />
+          <TicketChart
+            title="Chamados por prioridade"
+            data={ticketPriorityData}
+            onSliceClick={handleSliceClick}
+          />
+        </div>
+
+        {/* <TicketChart
           title="Chamados por departamento"
           data={ticketDepartmentData}
           type="bar"
@@ -92,11 +136,11 @@ function DashBoardFeature() {
           data={ticketMonthlyData}
           type="bar"
         /> */}
-			</div>
+      </div>
 
-			<TicketFeature />
-		</div>
-	);
+      <TicketFeature />
+    </div>
+  );
 }
 
 export default DashBoardFeature;
